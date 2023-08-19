f118572c7327ab4600576f475a3f750dc9718a94
aristides1000 <aristides1000@gmail.com>
2023-08-19 17:53:48 -0400

    feat: add functionality of to do add new task, edit, complete and delete

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let deleteTasks, editTasks, tasks;
 let updateNote = "";
 let count;
 
-// Function on window load
+//Function on window load
 window.onload = () => {
   updateNote = "";
   count = Object.keys(localStorage).length;
   displayTasks();
-}
+};
 
 //Function to Display The Tasks
 const displayTasks = () => {
@@ -23,7 +23,7 @@ const displayTasks = () => {
   //Clear the tasks
   tasksDiv.innerHTML = "";
 
-  //Fetch All the keys in local storage
+  //Fetch All The Keys in local storage
   let tasks = Object.keys(localStorage);
   tasks = tasks.sort();
 
@@ -37,7 +37,7 @@ const displayTasks = () => {
     taskInnerDiv.setAttribute("id", key);
     taskInnerDiv.innerHTML = `<span id="taskname">${key.split("_")[1]}</span>`;
 
-    //localstorage would store boolean as stringso we parse it to boolean back
+    //localstorage would store boolean as string so we parse it to boolean back
     let editButton = document.createElement("button");
     editButton.classList.add("edit");
     editButton.innerHTML = `<ion-icon name="create"></ion-icon>`;
@@ -62,24 +62,80 @@ const displayTasks = () => {
       } else {
         updateStorage(element.id.split("_")[0], element.innerText, true);
       }
-    }
+    };
   });
 
   //Edit Tasks
   editTasks = document.getElementsByClassName("edit");
   Array.from(editTasks).forEach((element, index) => {
     element.addEventListener("click", (e) => {
-      //Stop propogation to outer elements (if removed when we click delete eventuallyrhw click will move to parent)
+      //Stop propogation to outer elements (if removed when we click delete eventually rhw click will move to parent)
       e.stopPropagation();
       //disable other edit buttons when one task is being edited
       disableButtons(true);
       //update input value and remove div
       let parent = element.parentElement;
-      newTaskInput.value = parent.querySelector("taskname").innerHTML;
+      newTaskInput.value = parent.querySelector("#taskname").innerText;
       //set updateNote to the task that is being edited
       updateNote = parent.id;
       //remove task
       parent.remove();
     });
   });
-}
+
+  //Delete Tasks
+  deleteTasks = document.getElementsByClassName("delete");
+  Array.from(deleteTasks).forEach((element, index) => {
+    element.addEventListener("click", (e) => {
+      e.stopPropagation();
+      //Delete from local storage and remove div
+      let parent = element.parentElement;
+      removeTask(parent.id);
+      parent.remove();
+      count -= 1;
+    });
+  });
+};
+
+//Disable Edit Button
+const disableButtons = (bool) => {
+  let editButtons = document.getElementsByClassName("edit");
+  Array.from(editButtons).forEach((element) => {
+    element.disabled = bool;
+  });
+};
+
+//Remove Task from local storage
+const removeTask = (taskValue) => {
+  localStorage.removeItem(taskValue);
+  displayTasks();
+};
+
+//Add tasks to local storage
+const updateStorage = (index, taskValue, completed) => {
+  localStorage.setItem(`${index}_${taskValue}`, completed);
+  displayTasks();
+};
+
+//Function To Add New Task
+document.querySelector("#push").addEventListener("click", () => {
+  //Enable the edit button
+  disableButtons(false);
+  if (newTaskInput.value.length == 0) {
+    alert("Please Enter A Task");
+  } else {
+    //Store locally and display from local storage
+    if (updateNote == "") {
+      //new task
+      updateStorage(count, newTaskInput.value, false);
+    } else {
+      //update task
+      let existingCount = updateNote.split("_")[0];
+      removeTask(updateNote);
+      updateStorage(existingCount, newTaskInput.value, false);
+      updateNote = "";
+    }
+    count += 1;
+    newTaskInput.value = "";
+  }
+});
